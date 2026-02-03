a50f5e6f50a98d771fcc5d83447756846fba8dc6
upmanpransh <upmanprashun987@gmail.com>
2026-02-03 12:42:33 +0530

    update

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000", "http://localhost:5173", "https://voyabyte.com"],
         credentials: true,
     })
 );
